dc6da34a054fcd1dd23d9fce1bdd013a3fca9969
ejaman <leegm17@naver.com>
2023-03-28 21:48:03 +0900

    setup: set folder(use atomic design pattern)

---
+++ client/src/pages/game/stage.tsx
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled";
+import React from "react";
+
+const Stage = () => {
+	return <Container>stage</Container>;
+};
+
+export default Stage;
+
+const Container = styled.div``;
