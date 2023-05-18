ff45ddb569de6feef8171443fe15cb5bca4f89e4
임현홍 <bbnerino@gmail.com>
2023-05-18 17:15:19 +0900

    feat : WebConfig cors 설정 추가

--- server/src/main/java/com/dubu/party/common/config/WebConfig.java
+++ server/src/main/java/com/dubu/party/common/config/WebConfig.java
@@ -7,10 +7,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
+    public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
-                .allowedMethods("GET", "POST")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .maxAge(3000);
     }
 }
+
