b9cc3e83a99733df55472f774324e835dd5ba9b5
임현홍 <bbnerino@gmail.com>
2023-05-23 22:24:51 +0900

    fix : 좋아요 관련 response 변경

--- server/src/main/java/com/dubu/party/domain/article/db/repository/ArticleLikeRepository.java
+++ server/src/main/java/com/dubu/party/domain/article/db/repository/ArticleLikeRepository.java
@@ -9,6 +9,8 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional // JPA의 모든 데이터 변경은 트랜잭션 안에서 실행되어야 한다.
 public interface ArticleLikeRepository extends JpaRepository<ArticleLike, Long> {
     boolean existsByUserIdAndArticleId(Long userId, Long articleId);
-    ArticleLike findByUserIdAndArticleId(Long userId, Long articleId);
+
+    ArticleLike getByUserIdAndArticleId(Long userId, Long articleId);
 
 }
+// get 메서드는 식별자를 사용하여 엔티티를 바로 조회하고, find 메서드는 다양한 조건을 사용하여 엔티티를 조회합니다.
